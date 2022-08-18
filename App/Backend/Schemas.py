47704a1f00e1b0c30aae62ffe0aed3898779f74d
Adwait110901 <73949532+Adwait110901@users.noreply.github.com>
2022-08-19 00:19:22 +0530

    backend APIs Created

---
+++ App/Backend/Schemas.py
@@ -0,0 +1,7 @@
+from pydantic import BaseModel
+# from fastapi import FastAPI, File, UploadFile
+class InputSchema(BaseModel):
+    filename : str
+    
+class emailAddrSchema(BaseModel):
+    emailIdList :list[str]
